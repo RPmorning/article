ad4b911e9c452af8980ce3c0d0ef5b2b92476a6e
任鹏 <rp18382106376@outlook.com>
2018-02-27 10:13:43 +0800

    ss

---
+++ app/api/controller/Category.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: renpeng
+ * Date: 2018/2/27-0027
+ * Time: 10:05
+ */
+namespace app\api\controller;
+
+use think\Controller;
+
+class Category extends Controller
+{
+    
+}
